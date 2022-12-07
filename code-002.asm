3af5e2ffea2e9e18a51b77fa524c145866318c75
sdev0x <120042320+sdev0x@users.noreply.github.com>
2022-12-07 16:24:43 +0000

    hi hello asm

---
+++ code-002.asm
@@ -0,0 +1,23 @@
+; 1 + 2 + 3 + 4
+; 10 - 6 - 2
+; 50 - 40 + 10
+
+mov a , 1 
+mov b , 2
+add a , b 
+add a , 3 
+add a , 4 
+
+;;;;;;;;;;;
+
+mov c , 10
+mov d , 6 
+sub c , d 
+sub c , 2
+
+;;;;;;;;;;
+
+mov a , 50
+mov b , 40 
+sub a , b 
+add a , 10
