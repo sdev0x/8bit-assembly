2e1459128d31ccc23b3f02d92fbc816fd86e0062
sdev0x <120042320+sdev0x@users.noreply.github.com>
2023-02-23 13:52:27 +0300

    Create code-034.asm

---
+++ code-034.asm
@@ -0,0 +1,22 @@
+mov a,3
+mov b,3
+that:
+mov c,3
+xor c,4
+and d,0x34
+not a
+this:
+mov d,3
+shl d,c
+shr a,b
+cmp a,b
+je the
+sub a,b
+add d,c
+mul a
+div b
+the:
+cmp d,c
+je there
+there:
+hlt
